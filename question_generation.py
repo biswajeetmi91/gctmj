6b65d605ce3788a4d47f1f4380537b9227b7d097
rohan589 <rohan99@gmail.com>
2016-03-14 03:21:01 -0400

    just added comments

--- question_generation.py
+++ question_generation.py
@@ -15,6 +15,43 @@ import pprint
 # - Need to check for tenses possibly.
 # - Other basic stuff like case conversion.
 
+################################################ Remaining question types that need to be tackled (Rohan and Biswa are working on them now):
+
+# For now, we will focus on forming easy questions. ("not-so-good" questions) These are the questions in which we don't change 
+# any parts of the sentence and we don't drop any irrelevant details from the question. 
+# Later we will learn to a) drop the irrelevant details or b) change the language 
+# to make it a better question.
+
+# Sample sentence: Rohan is going to school by car along with his friend on Monday to work on the project.
+
+# What: 
+# Not-so-good question: What is Rohan going to school by car along with his friend on Monday for?
+# Better question: What is Rohan going to school on Monday for ?
+# Even better question : What is Rohan doing on Monday?
+
+# When: 
+# Not-so-good question: When is Rohan going to school by car along with his friend to work on the project?
+
+# Where:
+# Not-so-good question: Where is Rohan going by car along with his friend on Monday to work on the project?
+
+# Whom: ('Who' is also considered acceptable in place of 'Whom', but we will keep them seperate here)
+# Not-so-good question: Whom is Rohan going by car along with on Monday to work on the project?
+
+# Who:
+# Not-so-good question: Who is going to school by car along with his friend on Monday to work on the project?
+# Better question: Who is going to school on Monday?
+# (Maybe)Even better question: Who is going to school on Monday to work on the project? **OR** Who is going to work on the project on Monday?
+
+# Why:
+# Not-so-good question: Why is Rohan going to school by car along with his friend on Monday?
+# Good question: Why is Rohan going to school on Monday?
+
+# How:
+# Very Bad question: How is Rohan going to school by car on Monday to work on the project? (Answer: along with his friend. 
+# This is valid but not a good question)
+# Not-so-good question: How is Rohan going to school along with his friend on Monday to work on the project?
+
 with open("example_article.txt") as f:
 	tokenizer = PunktSentenceTokenizer()
 	sentences = tokenizer.tokenize(f.read().decode('utf-8').replace("\n"," "))
@@ -27,6 +64,7 @@ with open("example_article.txt") as f:
 	sentences[0] = "I have always wondered how I have always been so good on the guitar."
 	sentences[0] =  "Our dinner has been eaten by the dog."
 	sentences[0] = "Playing golf is my favorite pastime"
+	sentences[0] = "He plays golf for a living"
 	
 	sentences[0] = sentences[0].rstrip('.')
 	parseTree = list(parser.raw_parse((sentences[0])))
@@ -57,7 +95,7 @@ with open("example_article.txt") as f:
 		# each subtree has some leaves
 		# leaves can be single or multiple words depending on the POS. (POS are heirarchical so one word can even belong to multiple POS)
 		# for example a verb like 'is' may be tagged with both VP and VBZ. I'm only interested in the terminal POS and count the word once which is why
-		# i'm removing all phrase parts of speec
+		# i'm removing all phrase parts of speech.
 		leaves = s.leaves()
 		if len(leaves) == 1 and str(s.label()) not in posTags['phrases']:
 			leaf = leaves[0]
