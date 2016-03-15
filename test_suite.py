c9aa5e6af15643ac27e6c86c818b6a45ea0e09f6
Leonardo <lrmneves@poli.ufrj.br>
2016-03-15 17:46:18 -0400

    added test suite

---
+++ test_suite.py
@@ -0,0 +1,11 @@
+import question_answering
+
+
+
+''' Propaganda article test'''
+
+question_answering.answer_questions("propaganda_article.txt","propaganda_QA.txt")
+
+'''Buffon article test'''
+
+question_answering.answer_questions("beckham_article.txt", "beckham_QA.txt")
